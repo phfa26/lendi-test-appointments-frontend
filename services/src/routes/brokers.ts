3755f282feabdbb49eebc2cd74cf6d34e16941e8
Paulo Amaral <paulo.amaral@servian.com>
2022-05-24 22:07:16 +1000

    Sorts brokers by ID in ascending order

--- services/src/routes/brokers.ts
+++ services/src/routes/brokers.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
 
 const brokers = [
-  { id: 1, name: "Cam" },
-  { id: 2, name: "Jam" },
-  { id: 3, name: "Sam" },
-  { id: 4, name: "Ham" },
+	{ id: 1, name: "Cam" },
+	{ id: 2, name: "Jam" },
+	{ id: 3, name: "Sam" },
+	{ id: 4, name: "Ham" },
 ];
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send(brokers);
+	const sortedBrokers = brokers.sort((a, b) => {
+		return a.id - b.id;
+	});
+	res.send(sortedBrokers);
 });
 
 export default router;
