555a52224c388a0877fc22b41e2113f274ed43e4
Paulo Amaral <paulo.amaral@servian.com>
2022-05-24 22:03:24 +1000

    Sorts appointment by date in descending order

--- services/src/routes/appointments.ts
+++ services/src/routes/appointments.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
 const appointments = [
-  { id: 1, brokerId: 1, date: "15/10/2021" },
-  { id: 2, brokerId: 3, date: "22/11/2021" },
-  { id: 3, brokerId: 3, date: "23/11/2021" },
-  { id: 4, brokerId: 4, date: "10/5/2021" },
-  { id: 5, brokerId: 3, date: "10/5/2022" },
+	{ id: 1, brokerId: 1, date: "15/10/2021" },
+	{ id: 2, brokerId: 3, date: "22/11/2021" },
+	{ id: 3, brokerId: 3, date: "23/11/2021" },
+	{ id: 4, brokerId: 4, date: "10/5/2021" },
+	{ id: 5, brokerId: 3, date: "10/5/2022" },
 ];
 
 const router = Router();
@@ -13,5 +13,13 @@ const router = Router();
 export default router;
 
 router.get("/", (req, res) => {
-  res.send(appointments);
+	const sortedAppointments = appointments.sort((a, b) => {
+		const [dayA, monthA, yearA] = a.date.split("/");
+		const [dayB, monthB, yearB] = b.date.split("/");
+		const dateA = new Date(Number(yearA), Number(monthA) - 1, Number(dayA));
+		const dateB = new Date(Number(yearB), Number(monthB) - 1, Number(dayB));
+		return dateB.valueOf() - dateA.valueOf();
+	});
+
+	res.send(sortedAppointments);
 });
